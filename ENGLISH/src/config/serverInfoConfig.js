eb25d884bc24107fc4d382e990f9bbbc1d47aa84
VassterO <161985347+VassterO@users.noreply.github.com>
2024-07-16 05:11:12 +0200

    Fix server id being already established

--- ENGLISH/src/config/serverInfoConfig.js
+++ ENGLISH/src/config/serverInfoConfig.js
@@ -1,5 +1,5 @@
 export const serverInfoConfig = {
-    SERVER_ID: '67002', // Replace with your actual ServerID
+    SERVER_ID: 'ServerID', // Replace with your actual ServerID
     API_URL: `https://api.allorigins.win/raw?url=${encodeURIComponent(`https://api.scplist.kr/api/servers/67002`)}`,
     title: "Your Server Information",
     titleColor: "text-red-500",
--- SPANISH/src/config/serverInfoConfig.js
+++ SPANISH/src/config/serverInfoConfig.js
@@ -1,5 +1,5 @@
 export const serverInfoConfig = {
-    SERVER_ID: '67002', // Reemplaza con tu ServerID actual
+    SERVER_ID: 'ServerID', // Reemplaza con tu ServerID actual
     API_URL: `https://api.allorigins.win/raw?url=${encodeURIComponent(`https://api.scplist.kr/api/servers/67002`)}`,
     title: "Información del Servidor SCP:SL",
     titleColor: "text-red-500",
